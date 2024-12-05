9a7ca0b90c25594e8544285e1ee85e8faacedbbf
Danil <danil_ustinov@mail.ru>
2024-12-05 09:54:02 +0700

    throw error if somebody use old path in eslint extends

---
+++ index.js
@@ -0,0 +1,3 @@
+throw new Error(
+    "The path '@steroidsjs/eslint-config' is no longer supported. Use '@steroidsjs/eslint-config/frontend' for frontend or '@steroidsjs/eslint-config/backend' for backend."
+);
