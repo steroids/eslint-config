9bd0fbddc7a4104e0a0501e3cef272d7a888f2a5
Danil <danil_ustinov@mail.ru>
2025-07-18 15:01:40 +0700

    fix: remove plugin, update package.json

--- plugins/eslint-plugin-steroidsjs-backend/index.js
+++
@@ -1,8 +0,0 @@
-const requiredEnumToHelperRule = require("./lib/rules/required-enum-to-helper");
-const plugin = {
-    rules: {
-        "required-enum-to-helper": requiredEnumToHelperRule,
-    } ,
-};
-
-module.exports = plugin;
